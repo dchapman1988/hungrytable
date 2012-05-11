e790cfbf8356b2771943ed38a74bc852c8db3c40
Nicholas Fine <nicholas.fine@gmail.com>
2012-05-11 15:36:06 -0500

    DRY up initialization - Move initialization from restaurant and user into base class

--- lib/hungrytable/base.rb
+++ lib/hungrytable/base.rb
@@ -1,5 +1,25 @@
 module Hungrytable
   class Base
+    
+    def initialize
+      validate_environment_variables
+
+      @consumer = OAuth::Consumer.new(
+        ENV['OT_OAUTH_KEY'],
+        ENV['OT_OAUTH_SECRET'],
+        realm: 'http://www.opentable.com/',
+        site: 'https://secure.opentable.com/api/otapi_v2.ashx',
+        request_token_path: '',
+        authorize_path: '',
+        access_token_path: '',
+        oauth_token: '',
+        oauth_version: '1.0',
+        http_method: :get
+      )
+      
+      @access_token = OAuth::AccessToken.new(@consumer)
+    end
+    
     def validate_environment_variables
       raise "Please set the environment variable OT_OAUTH_KEY to your OpenTable OAuth Table." unless ENV['OT_OAUTH_KEY']
       raise "Please set the environment variable OT_OAUTH_SECRET to your OpenTable OAuth Secret Key." unless ENV['OT_OAUTH_SECRET']
--- lib/hungrytable/restaurant.rb
+++ lib/hungrytable/restaurant.rb
@@ -1,26 +1,5 @@
 module Hungrytable
   class Restaurant < ::Hungrytable::Base
-    def initialize
-      validate_environment_variables
-
-      @consumer = OAuth::Consumer.new(
-        ENV['OT_OAUTH_KEY'],
-        ENV['OT_OAUTH_SECRET'],
-        realm: 'http://www.opentable.com/',
-        site: 'https://secure.opentable.com/api/otapi_v2.ashx',
-        request_token_path: '',
-        authorize_path: '',
-        access_token_path: '',
-        oauth_token: '',
-        oauth_version: '1.0',
-        http_method: :get
-      )
-
-      @consumer.http.set_debug_output($stderr)
-      
-      @access_token = OAuth::AccessToken.new(@consumer)
-    end
-
     def get_details(restaurant_id)
       uri = "/restaurant/?pid=#{ENV['OT_PARTNER_ID']}&rid=#{restaurant_id}"
       response = @access_token.get(uri)
