2a5d6841b17a5adf855313452847b56b63fb8b00
Josh Adams <josh@isotope11.com>
2012-07-04 16:58:01 -0500

    Turn off junit reporter

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -13,9 +13,6 @@ Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
 # Set up minitest
 MiniTest::Unit.runner = MiniTest::SuiteRunner.new
 MiniTest::Unit.runner.reporters << MiniTest::Reporters::SpecReporter.new
-if ENV['JENKINS']
-  MiniTest::Unit.runner.reporters << MiniTest::Reporters::JUnitReporter.new
-end
 
 require 'mocha'
 require File.expand_path("../../lib/hungrytable.rb", __FILE__)
