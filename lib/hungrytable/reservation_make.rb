69044dd88b9e4a247af5cac72ab223b2431343df
David Chapman <dchapman1988@gmail.com>
2025-11-26 10:32:30 -0600

    lint

--- lib/hungrytable/reservation_make.rb
+++ lib/hungrytable/reservation_make.rb
@@ -53,8 +53,8 @@ module Hungrytable
     def params
       # Filter out internal options (like :requester) before converting to API parameters
       internal_opts = %i[requester]
-      api_opts = opts.reject { |key, _| internal_opts.include?(key) }
-      
+      api_opts = opts.except(*internal_opts)
+
       # Convert symbol keys to strings for API
       user_opts = api_opts.transform_keys(&:to_s)
       default_options.merge(user_opts)
